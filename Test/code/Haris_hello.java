806569d548a31048dd89b9989d5bf195878bc8d0
Mehinovic Haris <haris.mehinovic2001@gmail.com>
2020-11-14 12:13:13 +0100

    nouveau

--- Test/code/Haris_hello.java
+++ Test/code/Haris_hello.java
@@ -3,7 +3,7 @@ package code;
 public class Haris_hello {
 
 	public static void main(String[] args) {
-		System.out.println("Yo tout le monde squeezie");
+		System.out.println("Hello world");
 
 	}
 
