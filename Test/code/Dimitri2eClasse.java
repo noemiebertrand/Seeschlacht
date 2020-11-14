d67aa709836a74241a6b193d7b4d3eaffedfc474
Dimitri Legrand <dimitri.legrand57@gmail.com>
2020-11-14 12:17:44 +0100

    test Dimitri

---
+++ Test/code/Dimitri2eClasse.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Dimitri2eClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("j'essaye une 2e Classe");
+	}
+
+}
