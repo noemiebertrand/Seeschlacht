fa8632f989dd3cd13fed400d56d787ac67ffbd09
Dimitri Legrand <dimitri.legrand57@gmail.com>
2020-11-14 12:02:31 +0100

    pour tester

--- Test/code/hello.java
+++ Test/code/hello.java
@@ -6,6 +6,7 @@ public class hello {
 		// TODO Auto-generated method stub
 		System.out.println("Yo tout le monde");
 		System.out.println("Ca change");
+		
 	}
 
 }
---
+++ Test/code/maClasse.java
@@ -0,0 +1,10 @@
+package code;
+
+public class maClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("je fais un test, et je crois que ça fonctionne nickel");
+	}
+
+}
