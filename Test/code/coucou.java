f32c96b7a9ca67db79f4a0922da962940f00dd99
Dimitri Legrand <dimitri.legrand57@gmail.com>
2020-11-14 12:06:49 +0100

    good

--- Test/code/coucou.java
+++ Test/code/coucou.java
@@ -5,6 +5,7 @@ public class coucou {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("est ce que c'est bon ?");
+System.out.println("oui oui");
 	}
 
 }
