3a6cdf63a88ecb21c22b0bc120fcb9ca5b2ef6b7
Noemie Bertrand <bertrand.noemie44@gmail.com>
2020-11-20 18:29:25 +0100

    nom devient coordonnees et indique ou colorer

--- src/main/java/com/example/edu/Controller.java
+++ src/main/java/com/example/edu/Controller.java
@@ -7,18 +7,29 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class Controller {
+
 	@FXML
 	private Button b00;
 	Schiffe test = new Schiffe();
 	
 	 @FXML
 	 protected void handleSubmitButtonAction(ActionEvent event)
-	 {
-		 if (test.map[0][0] == '0') {
+	 { 	 String nom=b00.getId();
+		char [] char_variabel = nom.toCharArray();
+
+		for (int i=0;i<nom.length();i++) {
+			Character.getNumericValue(char_variabel[i]);}
+	 
+	 int[] int_variabel = new int[nom.length()];
+	 
+	 for (int i=0;i<nom.length();i++) {
+		 int_variabel[i]=Character.getNumericValue(char_variabel[i]);}
+
+		 if (test.map[int_variabel[1]][int_variabel[2]] == '*') {
 	 b00.setStyle("-fx-background-color:#965866;");
 		 }
 		 else {
 			 
 		 }
 	 }
-	}
+	 }
