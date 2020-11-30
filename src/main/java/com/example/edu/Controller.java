d5e6fda863d16b27e870f6fc2905bac552741d79
Dimitri Legrand <dimitri.legrand57@gmail.com>
2020-11-30 12:32:30 +0100

    solution en orange

--- src/main/java/com/example/edu/Controller.java
+++ src/main/java/com/example/edu/Controller.java
@@ -1,6 +1,9 @@
 package com.example.edu;
 
 
+import java.util.List;
+import java.util.Vector;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -105,24 +108,33 @@ public class Controller {
 		GameOver.setEffect(glow);
 		GameOver.setVisible(true);
 		
-		Button [] [] array = new Button [10] [10];
-		String buttonId;
-		Button boutonTest = null;
+		Button [] [] arrayButton = new Button [10] [10];
 		
-		array [0][0]=b00;
+		Button[] listButton = {
+				b00,b01,b02,b03,b04,b05,b06,b07,b08,b09,
+				b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,
+				b20,b21,b22,b23,b24,b25,b26,b27,b28,b29,
+				b30,b31,b32,b33,b34,b35,b36,b37,b38,b39,
+				b40,b41,b42,b43,b44,b45,b46,b47,b48,b49,
+				b50,b51,b52,b53,b54,b55,b56,b57,b58,b59,
+				b60,b61,b62,b63,b64,b65,b66,b67,b68,b69,
+				b70,b71,b72,b73,b74,b75,b76,b77,b78,b79,
+				b80,b81,b82,b83,b84,b85,b86,b87,b88,b89,
+				b90,b91,b92,b93,b94,b95,b96,b97,b98,b99};
+		
+		int k = 0;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				arrayButton [i][j] = listButton [k];
+				k++;
+			}
 				
+			}
+		
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				
-				if (test.map[i][j] != '*') {
-				
-					buttonId = "b"+i+j;
-					System.out.println(buttonId);
-					//bouton.setId(buttonId);
-					array[0][0].setStyle("-fx-background-color:ORANGE;");
-					
-					
-				}
+				if (test.map[i][j] != '*')	arrayButton[i][j].setStyle("-fx-background-color:ORANGE;");
 			}
 		}
 		
