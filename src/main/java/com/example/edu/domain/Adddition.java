a142c70ff5749586ec194a8c90616d4077dcf1a2
Noemie Bertrand <bertrand.noemie44@gmail.com>
2020-12-10 10:26:19 +0100

    added Addition Test JUnit

---
+++ src/main/java/com/example/edu/domain/Adddition.java
@@ -0,0 +1,24 @@
+
+package com.example.edu.domain;
+
+
+class Adddition {
+	double x=0;
+
+public Long calculer(Long a, Long b) {
+      return a+b;
+   }
+   public Character lireSymbole() {
+      return '-';
+   }
+   
+   public void core (Adddition b) {
+	   x=1; 
+	  b.getX();
+	  System.out.println("resl"+x);
+	  
+   }
+public double getX() {
+	return this.x;
+}
+}
